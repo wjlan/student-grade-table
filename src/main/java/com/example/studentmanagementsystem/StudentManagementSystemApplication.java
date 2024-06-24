b265b6097e959d3d376cfbec8f6fca93f307e8b5
wanjun <wanjunlan@gmail.com>
2024-06-24 16:36:54 -0700

    initial commit

---
+++ src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
